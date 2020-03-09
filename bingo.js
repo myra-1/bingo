03c7d262278c44ded6d10d988b08e94a7b3ed754
Myra Kornides <myra@Myras-MacBook-Pro.local>
2020-03-09 13:54:12 -0400

    Added numbers and arranged using flex display. Set-up page layout using grid

--- bingo.js
+++ bingo.js
@@ -1,10 +1,10 @@
-const nextButton = document.querySelector('#next');
+const nextButton = document.querySelector('#next-button');
 const funFact = document.querySelector('.fun-fact');
 
+
 nextButton.addEventListener('click', async () => {
+  funFact.innerHTML = "Loading";
   const response = await axios.get(`http://numbersapi.com/random/trivia`)
   const numberFact = response.data
-
   funFact.innerHTML = `<p> ${numberFact} </p>`;
-
 });
