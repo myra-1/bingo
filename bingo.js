e2bb275e130d009544e64614f11535a5444fa24e
Myra Kornides <myra@Myras-MBP.lan1>
2020-03-09 22:48:53 -0400

    just realized my last few commits were in the wrong file. luckily everything got better and resolved as i was committing, but kind of upset to lose the record of my progress

--- bingo.js
+++ bingo.js
@@ -1,10 +1,42 @@
+// variables
+// ============================================================================//
 const nextButton = document.querySelector('#next-button');
 const funFact = document.querySelector('.fun-fact');
+const currentDisplay = document.querySelector('#current-call-display');
+// const testButton = document.querySelector('#test-button');
+const olNeedToCallChildren = document.querySelector("#ol-need-to-call").children;
+// ^^ This selects all 75 <li> from the ol-need-to-call list
+const olHaveCalled = document.querySelector("#ol-have-called");
+let newListItem = document.createElement("li");
 
+// ============================================================================//
 
 nextButton.addEventListener('click', async () => {
+  let theCall = Math.floor(Math.random() * olNeedToCallChildren.length)
+  let theElement = olNeedToCallChildren[theCall];
+  console.log(theElement);
+  newListItem.appendChild(theElement);
+  // olHaveCalled.appendChild(newListItem);
+  // ^^ issues with order
+  olHaveCalled.insertBefore(theElement, olHaveCalled.firstChild);
+  //  ^^ this inserts in order
+  currentDisplay.innerHTML = theElement.innerHTML;
+  // ^^ setting this properties HTML to equal the elements innerHTML 
+
+  // API fun fact about number starts below
   funFact.innerHTML = "Loading";
-  const response = await axios.get(`http://numbersapi.com/random/trivia`)
+  let number = theElement.getAttribute("data-number");
+  const response = await axios.get(`http://numbersapi.com/${number}`)
   const numberFact = response.data
   funFact.innerHTML = `<p> ${numberFact} </p>`;
 });
+
+// ========================================================================================= //
+// ========================================================================================= //
+
+// nextButton.addEventListener('click', async () => {
+//   funFact.innerHTML = "Loading";
+//   const response = await axios.get(`http://numbersapi.com/random/trivia`)
+//   const numberFact = response.data
+//   funFact.innerHTML = `<p> ${numberFact} </p>`;
+// });
